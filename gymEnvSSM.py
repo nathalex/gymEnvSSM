45e708c2a5429ab0b353f705902ec168a639ec3b
nathalex <aloH0M0ra>
2021-08-10 16:41:02 +0100

    Spring Cleaning! (cleaned up commenting and spacing)

--- gymEnvSSM.py
+++ gymEnvSSM.py
@@ -1,5 +1,3 @@
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 import pygame
 import pymunk
 import pymunk.pygame_util
@@ -16,8 +14,11 @@ import cv2
 import GershbergSaxton as GS
 import manual_control
 from skimage.metrics import _structural_similarity as ssim
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
 
 # TODO: define a state where no more steps are necessary, i.e. the process is done (in step function)
+# TODO: when it's done, the coalitions must be output
 
 _image_library = {}
 
@@ -69,8 +70,8 @@ class raw_env(AECEnv, EzPickle):
         obs_height = 855 - 472
         obs_width = 625 - 37
 
-
-        assert self.element_width == self.wall_width, "Wall width and element width must be equal for observation calculation"
+        assert self.element_width == self.wall_width, \
+            "Wall width and element width must be equal for observation calculation"
         assert self.n_elements >= 1, "n_elements must be greater than 1"
 
         self.agents = ["element_" + str(r) for r in range(self.n_elements)]
@@ -87,7 +88,6 @@ class raw_env(AECEnv, EzPickle):
             self.action_spaces = dict(zip(self.agents, [gym.spaces.Discrete(3)] * self.n_elements))
         self.state_space = gym.spaces.Box(low=0, high=255, shape=(self.screen_height, self.screen_width, 3), dtype=np.uint8)
 
-
         pygame.init()
         pymunk.pygame_util.positive_y_is_up = False
 
@@ -100,10 +100,10 @@ class raw_env(AECEnv, EzPickle):
         self.background = get_image('background.png')
 
         self.elementList = []
-        self.elementPosVert = []  #Keeps track of vertical positions of elements
+        self.elementPosVert = []  # Keeps track of vertical positions of elements
         self.elementRewards = []     # Keeps track of individual rewards
         self.elementCoalitions = list(range(self.n_elements)) # Keeps track of which Coalition each element belongs to
-        self.current_phasemap = [] #current phasemap at any point
+        self.current_phasemap = []
         self.recentFrameLimit = 20  # Defines what "recent" means in terms of number of frames.
         self.recentelements = set()  # Set of elements that have touched the ball recently
         self.time_penalty = time_penalty
@@ -137,8 +137,8 @@ class raw_env(AECEnv, EzPickle):
 
     def observe(self, agent):
         observation = pygame.surfarray.pixels3d(self.screen)
-        i = self.agent_name_mapping[agent]
-        # values based on the background, the observation space includes all of the elements! it's a well-connected graph
+        # Values based on the background image
+        # The observation space includes all of the elements as it's a well-connected graph
         x_low = 37
         x_high = 625
         y_low = 472
@@ -149,9 +149,7 @@ class raw_env(AECEnv, EzPickle):
         return observation
 
     def state(self):
-        '''
-        Returns an observation of the global environment
-        '''
+        # Returns an observation of the global environment
         state = pygame.surfarray.pixels3d(self.screen).copy()
         state = np.rot90(state, k=3)
         state = np.fliplr(state)
@@ -198,7 +196,7 @@ class raw_env(AECEnv, EzPickle):
         return element
 
     def get_elem_coalition(self, elem):
-        # we first make sure all the elements in the coalition move as one
+        # We first make sure all the elements in the coalition move as one
         indices = [i for i, x in enumerate(self.elementCoalitions) if
                    x == self.elementCoalitions[self.elementList.index(elem)]]
         elements = []
@@ -207,27 +205,30 @@ class raw_env(AECEnv, EzPickle):
         return elements
 
     def pair_element(self, elem, action):
-        if action > 0: #pair agent with a random coalition
-            if action > 1: #in theory this never happens
+        if action > 0:
+            # Pair agent with a coalition
+            if action > 1:
+                # In theory this never happens, forces action between 0 and 1
                 action = action/math.ceil(action)
             coalition_to_join = int(action * len(self.elementList))
             elem_index = self.elementList.index(elem)
-            self.elementCoalitions[elem_index] = self.elementCoalitions[coalition_to_join]#[random_agent]
-        else: #move agent to its own coalition
+            self.elementCoalitions[elem_index] = self.elementCoalitions[coalition_to_join]
+        else:
+            # Move agent to its own coalition
             elem_index = self.elementList.index(elem)
-            self.elementCoalitions[elem_index] = elem_index #set coalition to element number
+            self.elementCoalitions[elem_index] = elem_index  # Set coalition to element number
 
-
-    def set_init_heights(self): #does the inverse of get_outputs and needs to be used in reset instead of setting random values!
-        self.elementCoalitions = list(range(self.n_elements)) # first sets all the coalitions back to the original one
-        # now goes through a list of heights per image and sets the element heights back to the expected heights * max height
+    def set_init_heights(self):
+        self.elementCoalitions = list(range(self.n_elements))  # First sets all the coalitions back to the original one
+        # Now goes through a list of heights per image
+        # Sets the element heights back to the expected heights * max height
         self.current_phasemap = list(self.phasemaps)
         for i in range(len(self.current_phasemap)):
             self.current_phasemap[i] = abs(self.current_phasemap[i]) * 3 * self.element_height
 
-
     def reset(self):
         self.set_init_heights()
+
         self.space = pymunk.Space(threaded=False)
         self.add_walls()
         self.space.gravity = (0.0, 750.0)
@@ -237,36 +238,33 @@ class raw_env(AECEnv, EzPickle):
         self.elementList = []
         self.elementPosVert = []
 
-
-        # display random phasemap
+        # Display a random phasemap
         self.map_to_display = self.np_random.choice(len(self.current_phasemap))
         display = self.current_phasemap[self.map_to_display]
 
         for i in range(int(math.sqrt(self.n_elements))):
             for j in range(int(math.sqrt(self.n_elements))):
 
-                #The elements need to move as a coalition!
-
-                if (j == 0):
-                    elemPos = self.screen_height - 434 + (i * 0.53 * self.element_height)
-                    maximum_element_y = elemPos - 3*self.element_height
+                # The elements need to move as a coalition
+                if j == 0:
+                    elem_pos = self.screen_height - 434 + (i * 0.53 * self.element_height)
+                    maximum_element_y = elem_pos - 3*self.element_height
                     element = self.add_element(
                         self.space,
-                        self.screen_width / 2 - self.element_width / 3.9 - (self.element_width * 0.67 * i), #x position
-                        maximum_element_y - display[i][j]
+                        self.screen_width / 2 - self.element_width / 3.9 - (self.element_width * 0.67 * i),  # x
+                        maximum_element_y - display[i][j]  # y position
                     )
                 else:
-                    elemPos = self.elementPosVert[len(self.elementPosVert) -1] + (0.59 * self.element_height)
-                    maximum_element_y = elemPos - 3*self.element_height
+                    elem_pos = self.elementPosVert[len(self.elementPosVert) - 1] + (0.59 * self.element_height)
+                    maximum_element_y = elem_pos - 3*self.element_height
                     element = self.add_element(
                         self.space,
-                        self.elementList[len(self.elementPosVert) - 1].position[0] + (self.element_width*0.65),#x position
-                        maximum_element_y - display[i][j]
+                        self.elementList[len(self.elementPosVert) - 1].position[0] + (self.element_width*0.65),  # x
+                        maximum_element_y - display[i][j]  # y position
                     )
                 element.velociy = 0
                 self.elementList.append(element)
-                self.elementPosVert.append(elemPos)
-
+                self.elementPosVert.append(elem_pos)
 
         self.draw_background()
         self.draw()
@@ -284,7 +282,6 @@ class raw_env(AECEnv, EzPickle):
         self.infos = dict(zip(self.agents, [{} for _ in self.agents]))
 
         self.frames = 0
-        #self.form_coalitions() #we actually form the coalitions in the step function!
 
     def draw_background(self):
 
@@ -337,7 +334,7 @@ class raw_env(AECEnv, EzPickle):
                 height + self.element_body_height * 1.2 - (self.screen_height - self.elementPosVert[i])
             )
             line3 = pygame.Rect(
-                element.position[0] + self.element_radius + self.element_width - 2,  # -2 to match up to element graphics
+                element.position[0] + self.element_radius + self.element_width - 2,  # -2 to match graphics
                 element.position[1] + self.element_radius + self.element_body_height / 2,
                 1,
                 height + self.element_body_height * 1.2 - (self.screen_height - self.elementPosVert[i])
@@ -350,7 +347,7 @@ class raw_env(AECEnv, EzPickle):
             self.screen.blit(self.element_sprite, (element.position[0] - self.element_radius, element.position[1] - self.element_radius - self.element_height / 2.5))
 
     def draw(self):
-        self.pre_compare(self.map_to_display) #update display post-coalitions
+        self.pre_compare(self.map_to_display)  # Update display post-coalitions
         self.draw_background()
 
         self.draw_elements()
@@ -358,21 +355,21 @@ class raw_env(AECEnv, EzPickle):
         font = pygame.font.SysFont(None, 12)
         white = (255, 255, 255)
         for element in self.elementList:
-            Coalition = font.render(str(self.elementCoalitions[self.elementList.index(element)]), True, white)
-            self.screen.blit(Coalition,
-                             (element.position[0] + self.element_width/2 - Coalition.get_width()/2, element.position[1] - Coalition.get_height()/2))
-
+            coalition = font.render(str(self.elementCoalitions[self.elementList.index(element)]), True, white)
+            self.screen.blit(coalition,
+                             (element.position[0] + self.element_width/2 - coalition.get_width()/2, element.position[1] - coalition.get_height()/2))
 
     def render(self, mode="human"):
         if not self.renderOn:
-            # sets self.renderOn to true and initializes display
+            # Sets self.renderOn to true and initializes display
             self.enable_render()
 
         observation = np.array(pygame.surfarray.pixels3d(self.screen))
         pygame.display.flip()
         return np.transpose(observation, axes=(1, 0, 2)) if mode == "rgb_array" else None
 
-    def pre_compare(self, pm): #sets the SSM to the required heights
+    def pre_compare(self, pm):
+        # Sets the SSM to the required heights
         non_visited_elements = list(range(len(self.elementList)))
         display = abs(self.phasemaps[pm]) * 3 * self.element_height
         disp_width = display.shape[0]
@@ -381,7 +378,7 @@ class raw_env(AECEnv, EzPickle):
                 elements = self.get_elem_coalition(self.elementList[i])
                 sum_heights = 0
                 vals_to_change = []
-                #calculate average height in display for all the elements:
+                # Calculate average height in display for all the elements:
                 for element in elements:
                     elem_index = self.elementList.index(element)
                     x = math.floor(elem_index/disp_width)
@@ -398,15 +395,12 @@ class raw_env(AECEnv, EzPickle):
                     self.current_phasemap[pm][vals_to_change[j][0]][vals_to_change[j][1]] = avg_height
                     non_visited_elements[elem_index] = -1
 
-
-
     def cost(self):
         max_num_coalitions = self.n_elements
         n_coalitions = len(set(self.elementCoalitions))
-        alpha = 1 #tuning parameter to control the cost, between 0 and 1
-        Cost = (n_coalitions * alpha)/max_num_coalitions #normalised between 0 and 1
-        return Cost
-
+        alpha = 1  # Tuning parameter to control the cost, between 0 and 1
+        cost = (n_coalitions * alpha)/max_num_coalitions  # Normalised between 0 and 1
+        return cost
 
     def prop(self, phasemap, cell_spacing, target_dist, res, k):
         aperture = ((phasemap != 0).astype(int))
@@ -415,26 +409,25 @@ class raw_env(AECEnv, EzPickle):
         return GS.ASM_fw(lpp, cell_spacing, target_dist, res, k)
 
     def ssim_metric(self, imageA, imageB):
-        ## Calculate the structural similarity metric between two images or arrays of equal size
-
+        # Calculate the structural similarity metric between two images or arrays of equal size
         s = ssim.structural_similarity(imageA, imageB)
         return s
 
     def SSIM(self, phasemap,i):
-        ##Structural Similarity Index Metric
-        ##comparing prop_px to prop_sg where phasemap is prop_px and prop_sg is the
+        # Structural Similarity Index Metric
+        # Comparing prop_px to prop_sg where phasemap is prop_px and prop_sg is the current SSM output
 
         cell_space = 1
         targ_dist = 0.5
         res_fac = 1
 
-        k = 2 * np.pi / 340  # wave number where all the waves are the same wavelength lambda = 340m 20Hz infrasound
+        k = 2 * np.pi / 340  # Wave number where all the waves are the same wavelength lambda = 340m 20Hz infrasound
 
-        #get current SSM's output phasemap and compare it to desired phasemap
-        current_prop = np.reshape(self.current_phasemap[i], phasemap.shape)# should reshape the phasemap
+        # Get current SSM's output phasemap and compare it to desired phasemap
+        current_prop = np.reshape(self.current_phasemap[i], phasemap.shape) # Reshape the phasemap
 
         max_height = 3*self.element_height
-        current_prop = current_prop/max_height #value between 0 and 1
+        current_prop = current_prop/max_height  # Value between 0 and 1
 
         current_prop = abs(self.prop(current_prop,cell_space,targ_dist,res_fac,k))
 
@@ -445,13 +438,13 @@ class raw_env(AECEnv, EzPickle):
         return SSIM
 
     def value(self):
-        sum = 0
+        sum_ssim = 0
         for i in range(len(self.phasemaps)):
             self.pre_compare(i)
-            sum += self.SSIM(self.phasemaps[i], i)
-        n_pt = len(self.phasemaps) #number of patterns
-        Value = sum/n_pt #value is the average SSIM over all the patterns
-        return Value
+            sum_ssim += self.SSIM(self.phasemaps[i], i)
+        n_pt = len(self.phasemaps)  # Number of patterns
+        value = sum_ssim/n_pt  # Value is the average SSIM over all the patterns
+        return value
 
     def reward(self):
         return self.value() - self.cost()
@@ -462,31 +455,25 @@ class raw_env(AECEnv, EzPickle):
         action = np.asarray(action)
         agent = self.agent_selection
 
-        #should we move an element if another member of its coalition moved prior?
-        # where do we give elements the option to leave the coalition or merge with another coalition?
-        # maybe we need to separate the reward function to include the cost consideration here?
-
-        #HERE: instead of moving the agents, we have them join coalitions! fun!
-
+        # Here, the agents join coalitions
         if self.continuous:
-            self.pair_element(self.elementList[self.agent_name_mapping[agent]], action) #pair element
+            self.pair_element(self.elementList[self.agent_name_mapping[agent]], action)  # Pair element
         else:
             self.pair_element(self.elementList[self.agent_name_mapping[agent]], action - 1)
-
-
 
         self.space.step(1 / 20.0)
         if self._agent_selector.is_last():
 
-            # Here, we check if the training is done (ie if the reward is no longer increasing for x = 1000ish iterations,
-            # or if the coalition formed is too large)
+            # Here, we check if the training is done (i.e. if the reward is no longer increasing for x iterations)
+            # if ball_min_x <= self.wall_width + 1:
+            #     self.done = True
 
             self.draw()
             local_reward = self.reward()
             global_reward = 0
             if not self.done:
                 global_reward += self.time_penalty
-            total_reward = [global_reward * (1 - self.local_ratio)] * self.n_elements  # start with global reward
+            total_reward = [global_reward * (1 - self.local_ratio)] * self.n_elements  # Start with global reward
             elements_to_reward = self.get_elem_coalition(self.elementList[self.agent_name_mapping[agent]])
             for elem in elements_to_reward:
                 total_reward[self.elementList.index(elem)] += local_reward * self.local_ratio
@@ -507,3 +494,7 @@ class raw_env(AECEnv, EzPickle):
         self.agent_selection = self._agent_selector.next()
         self._cumulative_rewards[agent] = 0
         self._accumulate_rewards()
+
+        if self.done:
+            # This is what we want out of the training!
+            return self.elementCoalitions
